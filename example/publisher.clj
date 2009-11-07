609ac692ed0c9ff6de67d94e39b365408b2c4997
Richard Newman <rnewman@twinql.com>
2009-11-06 20:45:03 -0800

    Fix filename (pubslisher -> publisher), align and indent examples.

--- example/publisher.clj
+++ example/publisher.clj
@@ -1,15 +1,15 @@
 (ns rabbitmq.publisher
   (:require [com.github.icylisper.rabbitmq :as rabbitmq]))
 
-(defonce conn-map { :username "guest"
-		    :password "guest"
-		    :host "localhost"
-		    :port 5672
-		    :virtual-host "/"
-		    :type "direct"
-		    :exchange "sorting-room"
-		    :queue "po-box"
-		    :routing-key "tata"})
+(defonce conn-map {:username "guest"
+                   :password "guest"
+                   :host "localhost"
+                   :port 5672
+                   :virtual-host "/"
+                   :type "direct"
+                   :exchange "sorting-room"
+                   :queue "po-box"
+                   :routing-key "tata"})
 
 (defonce connection (rabbitmq/connect conn-map))
 
@@ -23,9 +23,7 @@
   (let [[conn channel] connection]
     (do
       (rabbitmq/bind-channel conn-map channel)
-      (println "rabbitmq publishing:"  (format "message%d" @c))
+      (println "rabbitmq publishing:" (format "message%d" @c))
       (rabbitmq/publish conn-map channel (format "message%d" @c))))
   
   (Thread/sleep 1000))
-
-
