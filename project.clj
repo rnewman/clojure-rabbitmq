569beba234fb35e9126c93b805b4a97cc7f07cfd
Isaac Praveen Abraham <icylisper@gmail.com>
2010-01-17 17:34:28 +0530

    remove dev-dependencies

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
                  [commons-io "1.2"]
                  [commons-cli "1.1"]
                  [rabbitmq-client "1.7.0"]]
-  
-  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
+    ; :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]]
+  )
