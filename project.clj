d845687427c5a4d453e38872f58ff4b0e0dd1a4d
Richard Newman <rnewman@twinql.com>
2010-05-13 17:23:20 -0700

    Use upstream 1.7.2 RabbitMQ client; latest Commons IO and CLI; bump version to 0.3.2.

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject clojure-rabbitmq "0.3.1"
-  :description "rabbitmq client in clojure"
+(defproject clojure-rabbitmq "0.3.2"
+  :description "RabbitMQ client in Clojure."
+  :repositories [["mvnrepository" "http://mvnrepository.com/"]]
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [commons-io "1.2"]
-                 [commons-cli "1.1"]
-                 [org.clojars.icylisper/rabbitmq-client "1.7.0"]]
-  ;:dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [commons-io "1.4"]
+                 [commons-cli "1.2"]
+                 [com.rabbitmq/amqp-client "1.7.2"]
+                 ; [org.clojars.icylisper/rabbitmq-client "1.7.0"]
+                 ]
+  ;:dev-dependencies [[leiningen/lein-swank "1.1.0"]]
   )
